75fbdbea04057a94cb137d7ee4425c36ece2318c
Lívia Nascimento <liviapnd@gmail.com>
2023-01-24 14:57:29 -0300

    Create 1012.py

---
+++ iniciante/1012.py
@@ -0,0 +1,16 @@
+a, b, c = input().split(" ")
+a = float(a)
+b = float(b)
+c = float(c)
+
+triangulo = (a*c)/2
+circulo = 3.14159 * c ** 2
+trapezio = ((a+b)*c)/2
+quadrado = b ** 2
+retangulo = a * b
+
+print("TRIANGULO: {:.3f}".format(triangulo))
+print("CIRCULO: {:.3f}".format(circulo))
+print("TRAPEZIO: {:.3f}".format(trapezio))
+print("QUADRADO: {:.3f}".format(quadrado))
+print("RETANGULO: {:.3f}".format(retangulo))
