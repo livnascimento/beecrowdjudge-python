507d3e1e3d13d35274f6d571c8663e00e3f5c274
Lívia Nascimento <liviapnd@gmail.com>
2023-02-19 11:11:46 -0300

    Final 1049

---
+++ iniciante/1049.py
@@ -0,0 +1,26 @@
+grupo = input()
+classe = input()
+alimentacao = input()
+
+if (grupo == "vertebrado"):
+    if (classe == "ave"):
+        if (alimentacao == "carnivoro"):
+            print("aguia")
+        else:
+            print("pomba")
+    else:
+        if (alimentacao == "onivoro"):
+            print("homem")
+        else:
+            print("vaca")
+else:
+    if (classe == "inseto"):
+        if (alimentacao == "hematofago"):
+            print("pulga")
+        else:
+            print("lagarta")
+    else:
+        if (alimentacao == "hematofago"):
+            print("sanguessuga")
+        else:
+            print("minhoca")
